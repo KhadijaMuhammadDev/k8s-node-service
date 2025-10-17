9d407a5e4850a9a3c124abf89f6dbaa0f4d85efe
Kadija <ms.khadijamuhammad@gmail.com>
2025-10-17 13:27:54 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:alpine
+
+WORKDIR /app
+
+EXPOSE 3000
+
+COPY package.json package-lock.json ./
+
+COPY . ./
+CMD ["npm", "start"]
---
+++ index.mjs
@@ -0,0 +1,24 @@
+import express from 'express'
+import os from 'os'
+
+const app = express()
+const PORT = 3000
+
+app.get("/", (req,res)=>{
+  
+    const message=`Hello KHadija , Iam POd ${os.hostname()}`
+    res.send(message)
+
+
+
+
+
+})
+
+app.listen(PORT, ()=>{
+    
+    console.log(`Web Server is listing at port ${PORT}`)
+    console.log(os.hostname())  
+
+
+})
